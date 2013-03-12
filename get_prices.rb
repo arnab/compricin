ed7088cb0916cac184676a4fc1e7b9189d0d35fa
Arnab Deka <arnab.deka@livingsocial.com>
2013-03-12 18:56:57 +0530

    add junglee and ebay

--- get_prices.rb
+++ get_prices.rb
@@ -5,39 +5,51 @@ require 'mechanize'
 require 'pry'
 
 stores = {
-  ebay: {},
-  infibeam: {},
   flipkart: {
     search_url: "http://www.flipkart.com/search/a/all?query=%s",
     item_css_path: ".fk-product-thumb",
-    price_css_path: ".fk-price .price",
     title_css_path: "a.fk-anchor-link",
+    price_css_path: ".fk-price .price",
   },
-  junglee: {}
+  junglee: {
+    search_url: "http://www.junglee.com/mn/search/junglee?field-keywords=%s",
+    item_css_path: ".result.product",
+    title_css_path: ".data a.title",
+    price_css_path: ".data .price",
+  },
+  ebay: {
+    search_url: "http://search.ebay.in/%s",
+    item_css_path: "#ResultSetItems *[itemprop=offers]",
+    title_css_path: "a.vip",
+    price_css_path: "*[itemprop=price]",
+  },
+  infibeam: {},
 }
 
 def fetch_and_display_prices_from(store_name, store_metadata, search_term)
   search_url = store_metadata[:search_url]
   search_url = search_url.gsub("%s", search_term)
-  puts "Looking up #{store_name}: #{search_url}"
+  puts "\n#{store_name}: #{search_url}"
 
 
   agent = Mechanize.new
   page = agent.get(search_url)
-  search_for_item_and_price(page, store_metadata)
+  search_for_item_and_price(page, store_metadata).each do |title, price|
+    puts sprintf("  %-100s    %10s", title, price)
+  end
 end
 
 def search_for_item_and_price(page, store_metadata)
   items = page.search(store_metadata[:item_css_path])
-  items.each do |item|
+  items.map do |item|
     title = item.search(store_metadata[:title_css_path]).text.rstrip.lstrip
     price = item.search(store_metadata[:price_css_path]).text.rstrip.lstrip
-    pp [title, price]
+    [title, price]
   end
 end
 
-
 search_term = ARGV[0] || "huggies"
+puts "search for: #{search_term}"
 stores.each_pair do |store, store_metadata|
   next if store_metadata.empty?
   fetch_and_display_prices_from(store, store_metadata, search_term)
